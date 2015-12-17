5d36b793b71db34043510cb34696d03b844ca5a0
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2015-12-17 10:58:42 -0800

    Stop using tabs!

--- octoprint_periodic_command/__init__.py
+++ octoprint_periodic_command/__init__.py
@@ -8,30 +8,30 @@ from octoprint.events import Events
 class PeriodicCommand(octoprint.plugin.SettingsPlugin,
                       octoprint.plugin.EventHandlerPlugin):
 
-	def __init__(self):
-		self._timer = None
+    def __init__(self):
+        self._timer = None
         
     def get_settings_defaults(self):
         return dict(periodicCommand="curl -o /tmp/print.jpg "http://localhost:8080/?action=snapshot" && mpack -s "Progress of your print" /tmp/print.jpg gustavo@gustavo.eng.br",
                     periodicPeriod=60)
 
-	def on_event(self, event, payload):
+    def on_event(self, event, payload):
         if event == PRINT_STARTED:
-    		if self._timer is None:
-        		self._timer = RepeatedTimer(int(self._settings.get(["periodicPeriod"])) * 60, self._timer_task)
-        		self._timer.start()
+            if self._timer is None:
+                self._timer = RepeatedTimer(int(self._settings.get(["periodicPeriod"])) * 60, self._timer_task)
+                self._timer.start()
         elif self._timer is not None and event in [Events.PRINT_CANCELLED, Events.PRINT_DONE, Events.FAILED]:
-			self._timer.cancel()
-			self._timer = None
+            self._timer.cancel()
+            self._timer = None
 
-	def _timer_task(self):
+    def _timer_task(self):
         command = self._settings.get(["periodicCommand"])
-		self._logger.info("Executing system with command: {command}".format(command=command))
-		try:
-			import sarge
-			p = sarge.run(command, async=True)
-		except Exception as e:
-			self._logger.exception("Error when shutting down: {error}".format(error=e))
-			return
+        self._logger.info("Executing system with command: {command}".format(command=command))
+        try:
+            import sarge
+            p = sarge.run(command, async=True)
+        except Exception as e:
+            self._logger.exception("Error when shutting down: {error}".format(error=e))
+            return
 
 __plugin_implementation__ = PeriodicCommand()
